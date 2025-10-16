72406060b83abeb41eaab824d5bfa19e707aec2e
1120365 <s1120365@dmhs.kh.edu.tw>
2025-10-16 14:41:58 +0800

    hw5

--- test1/main.cpp
+++ test1/main.cpp
@@ -2,10 +2,29 @@
 using namespace std;
 
 int main() {
-    int sum = 0;
-    for (int i = 1; i <= 50; i++) {
-        if ((i % 7) == 0) 
-            cout << i << " ";
+    // Part 1: Star Pattern
+    cout << "=== Star Pattern ===" << endl;
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
     }
+
+    cout << endl; // spacing between parts
+
+    // Part 2: Multiplication Table (1~9)
+    cout << "=== Multiplication Table ===" << endl;
+    int i = 1;
+    while (i <= 9) {
+        int j = 1;
+        while (j <= 9) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
     return 0;
 }
