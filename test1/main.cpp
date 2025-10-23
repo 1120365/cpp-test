dc23da1d063d1d7fca286cf8f04a77bc203ea9b5
1120365 <s1120365@dmhs.kh.edu.tw>
2025-10-23 14:43:55 +0800

    hw6

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    // Part 1: Star Pattern
-    cout << "=== Star Pattern ===" << endl;
-    for (int i = 1; i <= 6; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            cout << "*";
+    int score[10] = {85, 90, 60, 92, 100, 76, 50, 89, 84, 40};
+    int gradeCount[5] = {0}; // To count grades A, B, C, D, F
+
+    // Categorizing scores into grades
+    for (int i = 0; i < 10; ++i) {
+        if (score[i] >= 90) {
+            gradeCount[0]++;
+        } else if (score[i] >= 80) {
+            gradeCount[1]++;
+        } else if (score[i] >= 70) {
+            gradeCount[2]++;
+        } else if (score[i] >= 60) {
+            gradeCount[3]++;
+        } else {
+            gradeCount[4]++;
         }
-        cout << endl;
     }
 
-    cout << endl; // spacing between parts
-
-    // Part 2: Multiplication Table (1~9)
-    cout << "=== Multiplication Table ===" << endl;
-    int i = 1;
-    while (i <= 9) {
-        int j = 1;
-        while (j <= 9) {
-            cout << i << " * " << j << " = " << i * j << "\t";
-            j++;
-        }
-        cout << endl;
-        i++;
-    }
+    // Outputting the count of each grade category
+    cout << "A等人數: " << gradeCount[0] << endl;
+    cout << "B等人數: " << gradeCount[1] << endl;
+    cout << "C等人數: " << gradeCount[2] << endl;
+    cout << "D等人數: " << gradeCount[3] << endl;
+    cout << "F等人數: " << gradeCount[4] << endl;
 
     return 0;
 }
